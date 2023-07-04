3d57a27149fcbe774947bac85827dd17550b457a
marie-fourier <sobirovutkir@gmail.com>
2023-07-04 19:04:43 +0500

    feat: use etherscan for optimism gas fees

--- packages/executor/src/utils/getGasFee.ts
+++ packages/executor/src/utils/getGasFee.ts
@@ -19,6 +19,16 @@ export const getGasFee = async (
       console.error(`Couldn't fetch fee data for matic: ${err}`);
     }
   }
+
+  if (network === "optimism") {
+    try {
+      return getOptimismGasFee();
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(`Couldn't fetch fee data for optimism: ${err}`);
+    }
+  }
+
   try {
     const feeData = await provider.getFeeData();
     return {
@@ -51,3 +61,23 @@ export const getMaticGasFee = async (): Promise<IGetGasFeeResult> => {
     gasPrice: parseGwei(data.fast.maxFee),
   };
 };
+
+export const getEtherscanGasFee = async (
+  apiUrl: string,
+  apiKey: string | undefined = undefined
+): Promise<IGetGasFeeResult> => {
+  const oracle =
+    `${apiUrl}?module=proxy&action=eth_gasPrice` + apiKey
+      ? ""
+      : `&apikey=${apiKey}`;
+  const data = await (await fetch(oracle)).json();
+  const gasPrice = ethers.BigNumber.from(data.result);
+  return {
+    maxPriorityFeePerGas: gasPrice,
+    maxFeePerGas: gasPrice,
+    gasPrice: gasPrice,
+  };
+};
+
+export const getOptimismGasFee = (): Promise<IGetGasFeeResult> =>
+  getEtherscanGasFee("https://api-optimistic.etherscan.io/api");
