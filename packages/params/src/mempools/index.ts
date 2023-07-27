629e2ffb28d39a3969d363aa7dcbc6ed46e0c9ba
marie-fourier <sobirovutkir@gmail.com>
2023-07-27 18:51:39 +0500

    add goerli mempools

--- packages/params/src/mempools/index.ts
+++ packages/params/src/mempools/index.ts
@@ -2,9 +2,11 @@ import { IMempoolsConfig } from "../types";
 import mumbaiMempools from "./mumbai";
 import sepoliaMempools from "./sepolia";
 import devMempools from "./dev";
+import goerliMempools from "./goerli";
 
 export const mempoolsConfig: IMempoolsConfig = {
   mumbai: mumbaiMempools,
   sepolia: sepoliaMempools,
   dev: devMempools,
+  goerli: goerliMempools,
 };
