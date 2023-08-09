5434aa036c98a4ff0421cd61ca2a4d6f9287757e
marie-fourier <sobirovutkir@gmail.com>
2023-08-09 19:21:29 +0500

    remove unused config param

--- packages/executor/src/interfaces.ts
+++ packages/executor/src/interfaces.ts
@@ -99,10 +99,6 @@ export interface NetworkConfig {
   banSlack: number;
   minSignerBalance: BigNumberish;
   multicall: string;
-  // reduces baseFee by a given number in % before dividing paid gas
-  // use this as a buffer to callGasLimit
-  // 25% by default
-  estimationBaseFeeDivisor: number;
   // adds certain amount of gas to callGasLimit
   // 21000 by default
   estimationStaticBuffer: number;
