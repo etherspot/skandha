cb8f1a7cbf73aa5ab1ef8ceed9a2bc99cc543e5d
marie-fourier <sobirovutkir@gmail.com>
2023-08-14 15:32:48 +0500

    feat: add mantle gas oracle

---
+++ packages/executor/src/utils/gas-oracles/mantle.ts
@@ -0,0 +1,28 @@
+import { BigNumber, ethers } from "ethers";
+import { MantleGasOracleABI } from "types/lib/executor/abis";
+import { IGetGasFeeResult, IOracle } from "./interfaces";
+
+const oracleAddress = "0x420000000000000000000000000000000000000F";
+const minGasPrice = 50000000;
+
+export const getMantleGasFee: IOracle = async (
+  apiKey,
+  provider
+): Promise<IGetGasFeeResult> => {
+  const oracle = new ethers.Contract(
+    oracleAddress,
+    MantleGasOracleABI,
+    provider
+  );
+
+  let gasPrice = await oracle.callStatic.gasPrice();
+  if (gasPrice && BigNumber.from(gasPrice).lt(minGasPrice)) {
+    gasPrice = BigNumber.from(minGasPrice);
+  }
+
+  return {
+    maxPriorityFeePerGas: gasPrice,
+    gasPrice: gasPrice,
+    maxFeePerGas: gasPrice,
+  };
+};
