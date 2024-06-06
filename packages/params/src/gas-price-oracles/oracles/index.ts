96fc64361f9fdc5d83820a53b64244e146a34a17
marie-fourier <sobirovutkir@gmail.com>
2024-06-06 17:34:36 +0500

    fix arbitrum gas oracle & add fallbacks

--- packages/params/src/gas-price-oracles/oracles/index.ts
+++ packages/params/src/gas-price-oracles/oracles/index.ts
@@ -7,19 +7,21 @@ import { getMumbaiGasFee } from "./mumbai";
 import { getOptimismGasFee } from "./optimism";
 import { IOracle } from "./interfaces";
 import { getMantleGasFee } from "./mantle";
-import { getAncient8GasFee } from "./ancient8";
 import { getBaseGasFee } from "./base";
+import { getAncient8GasFee } from "./ancient8";
+import { getEthGasPrice } from "./eth_gasPrice";
 
 export const oracles: {
-  [chainId: number]: IOracle | undefined;
+  [chainId: number]: IOracle | IOracle[] | undefined;
 } = {
   137: getMaticGasFee,
   80001: getMumbaiGasFee,
-  10: getOptimismGasFee,
-  42161: getArbitrumGasFee,
+  10: [getOptimismGasFee, getEthGasPrice],
+  42161: [getArbitrumGasFee, getEthGasPrice],
   5000: getMantleGasFee,
   5001: getMantleGasFee,
+  8453: [getBaseGasFee, getEthGasPrice],
   888888888: getAncient8GasFee,
-  8453: getBaseGasFee,
+  59144: getEthGasPrice,
   5003: getMantleGasFee,
 };
