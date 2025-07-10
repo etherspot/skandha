f9bea975907387768c4b98f522ba2e2b03a4af62
Nikhil Kumar <48001923+nikhilkumar1612@users.noreply.github.com>
2025-07-10 15:21:18 +0530

    feat: flashbots relay (#324)

---
+++ packages/executor/src/services/BundlingService/relayers/flashbots.ts
@@ -0,0 +1,175 @@
+import { PerChainMetrics } from "@skandha/monitoring/lib";
+import { Logger } from "@skandha/types/lib";
+import { Config } from "../../../config";
+import { Bundle, NetworkConfig } from "../../../interfaces";
+import { MempoolService } from "../../MempoolService";
+import { ReputationService } from "../../ReputationService";
+import { estimateBundleGasLimit } from "../utils";
+import { Relayer } from "../interfaces";
+import { ExecutorEventBus } from "../../SubscriptionService";
+import { EntryPointService } from "../../EntryPointService";
+import { BaseRelayer } from "./base";
+import { AuthorizationList, Hex, hexToBytes, keccak256, LocalAccount, PublicClient, toHex, TransactionRequest, WalletClient } from "viem";
+import axios from "axios";
+import { getAuthorizationList } from "../utils/eip7702";
+
+export class FlashbotsRelayer extends BaseRelayer {
+  constructor(
+    logger: Logger,
+    chainId: number,
+    publicClient: PublicClient,
+    config: Config,
+    networkConfig: NetworkConfig,
+    entryPointService: EntryPointService,
+    mempoolService: MempoolService,
+    reputationService: ReputationService,
+    eventBus: ExecutorEventBus,
+    metrics: PerChainMetrics | null
+  ) {
+    super(
+      logger,
+      chainId,
+      publicClient,
+      config,
+      networkConfig,
+      entryPointService,
+      mempoolService,
+      reputationService,
+      eventBus,
+      metrics
+    );
+    if (!this.networkConfig.rpcEndpointSubmit) {
+      throw Error(
+      "If you want to use Flashbots Builder API, please set API url in 'rpcEndpointSubmit' in config file"
+      );
+    }
+  }
+
+  async sendBundle(bundle: Bundle): Promise<void> {
+    const availableIndex = this.getAvailableRelayerIndex();
+    if (availableIndex == null) return;
+
+    const relayer = this.relayers[availableIndex];
+    const mutex = this.mutexes[availableIndex];
+
+    const { entries } = bundle;
+    if (!bundle.entries.length) return;
+
+    await mutex.runExclusive(async (): Promise<void> => {
+      const beneficiary = await this.selectBeneficiary(relayer);
+      const entryPoint = entries[0]!.entryPoint;
+      const txRequest = this.entryPointService.encodeHandleOps(
+        entryPoint,
+        entries.map((entry) => entry.userOp),
+        beneficiary
+      );
+
+      const { authorizationList, rpcAuthorizationList } =
+              getAuthorizationList(bundle);
+
+      const transactionRequest: TransactionRequest = {
+        to: entryPoint as Hex,
+        data: txRequest,
+        type: authorizationList.length > 0 ? "eip7702" : "eip1559",
+        maxPriorityFeePerGas: BigInt(bundle.maxPriorityFeePerGas),
+        maxFeePerGas: BigInt(bundle.maxFeePerGas),
+        gas: estimateBundleGasLimit(
+          this.networkConfig.bundleGasLimitMarkup,
+          bundle.entries,
+          this.networkConfig.estimationGasLimit
+        ),
+        nonce: await this.publicClient.getTransactionCount({address: relayer.account?.address!}),
+      };
+
+      if (!(await this.validateBundle(relayer, entries, transactionRequest, rpcAuthorizationList))) {
+        return;
+      }
+
+      await this.submitTransaction(relayer, transactionRequest, authorizationList)
+        .then(async (txHash) => {
+          this.logger.debug(`Flashbots: Bundle submitted: ${txHash}`);
+          this.logger.debug(
+            `Flashbots: User op hashes ${entries.map(
+              (entry) => entry.userOpHash
+            )}`
+          );
+          await this.setSubmitted(entries, txHash);
+          await this.waitForEntries(entries).catch((err) =>
+            this.logger.error(err, "Flashbots: Could not find transaction")
+          );
+          this.reportSubmittedUserops(txHash, bundle);
+        })
+        .catch(async (err: any) => {
+          this.reportFailedBundle();
+          // Put all userops back to the mempool
+          // if some userop failed, it will be deleted inside handleUserOpFail()
+          await this.setNew(entries);
+          if (err === "timeout") {
+            this.logger.debug("Flashbots: Timeout");
+            return;
+          }
+          await this.handleUserOpFail(entries, err);
+          return;
+        });
+    });
+  }
+
+  /**
+   * signs & sends a transaction
+   * @param signer wallet
+   * @param transaction transaction request
+   * @param storageMap storage map
+   * @returns transaction hash
+   */
+  private async submitTransaction(
+    signer: Relayer,
+    transaction: TransactionRequest,
+    authorizationList: AuthorizationList
+  ): Promise<string> {
+    try {
+      this.logger.debug(transaction, "Flashbots: Submitting");
+      const signedTransaction = await signer.signTransaction({...transaction, authorizationList} as any);
+      const validBlockNumber = toHex((await this.publicClient.getBlockNumber()) + BigInt(5));
+      
+      const data = JSON.stringify({
+        jsonrpc: "2.0",
+        method: "eth_sendBundle",
+        params: [{
+          txs:[signedTransaction],
+          blockNumber: validBlockNumber,
+        }],
+        id: 1
+      });
+
+      const payloadSignature = await (signer.account as LocalAccount<'privateKey'>).signMessage({
+        message: keccak256(toHex(data))
+      });
+      const signature = signer.account?.address! + ':' + payloadSignature;
+
+      const config = {
+        method: "post",
+        url: this.networkConfig.rpcEndpointSubmit,
+        headers: {
+          "Content-Type": "application/json",
+          "X-Flashbots-Signature": signature,
+        },
+        data
+      };
+      return await axios.request(config).then((response) => {
+        const { error } = response.data;
+        this.logger.info(response.data, "Flashbots: Bundle response");
+        if(error) {
+          this.logger.error(error, "Flashbots: Error submitting bundle");
+          throw new Error(error);
+        }
+        return keccak256(hexToBytes(signedTransaction));
+      }).catch((err) => {
+        this.logger.error(err, "Flashbots: Error submitting bundle");
+        throw err;
+      })
+    } catch (error) {
+      this.logger.error(error, "Flashbots: Error submitting bundle");
+      throw error;
+    }
+  }
+}
