3e813e7369fd09c2f337903114af4646f60c9d9a
Nikhil Kumar <48001923+nikhilkumar1612@users.noreply.github.com>
2025-08-20 18:36:04 +0530

    fix: removing block number validity for eth_sendBundle calls (#332)

--- packages/executor/src/services/BundlingService/relayers/flashbots.ts
+++ packages/executor/src/services/BundlingService/relayers/flashbots.ts
@@ -129,14 +129,12 @@ export class FlashbotsRelayer extends BaseRelayer {
     try {
       this.logger.debug(transaction, "Flashbots: Submitting");
       const signedTransaction = await signer.signTransaction({...transaction, authorizationList} as any);
-      const validBlockNumber = toHex((await this.publicClient.getBlockNumber()) + BigInt(5));
       
       const data = JSON.stringify({
         jsonrpc: "2.0",
         method: "eth_sendBundle",
         params: [{
           txs:[signedTransaction],
-          blockNumber: validBlockNumber,
         }],
         id: 1
       });
