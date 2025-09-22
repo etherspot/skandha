ed605af7aeebccc24da486772b468c0530d99651
Nikhil Kumar <48001923+nikhilkumar1612@users.noreply.github.com>
2025-09-22 14:25:03 +0530

    fix: minor fix estimation, inc default gas for debug trace call, use … (#347)

--- packages/executor/src/services/EntryPointService/versions/0.0.8.ts
+++ packages/executor/src/services/EntryPointService/versions/0.0.8.ts
@@ -615,9 +615,9 @@ export class EntryPointV8Service implements IEntryPointService {
             [this.address.toLowerCase()]: {
               code: _deployedBytecode,
             },
-            [userOp.sender]: {
+            [userOp.sender.toLowerCase()]: {
               code: ("0xef0100" +
-                userOp.eip7702Auth.address.substring(2)) as Hex,
+                userOp.eip7702Auth.address.substring(2)).toLowerCase() as Hex,
             },
           },
         ];
